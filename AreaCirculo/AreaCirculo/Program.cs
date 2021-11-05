a3f25a9594ac249ad7d58ada2f5e7de93ba0578a
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-05 09:54:15 -0300

    Exercício
    Feito e conferido no URI

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double raio, area, pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

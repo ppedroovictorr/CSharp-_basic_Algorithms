48db23e98ea2bbca49f46574195f486845b1e2d3
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-05 09:58:39 -0300

    Add files via upload
    Feito durante curso UDEMY

---
+++ ValorTerreno/Curso/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, valor;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = largura * comprimento;
+            double PrecoMetroQuadrado = area * valor;
+
+            Console.WriteLine("Área do terreno: " + area.ToString("F2") + " m2", CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor do terreno: " + PrecoMetroQuadrado.ToString("F2") + " reais.", CultureInfo.InvariantCulture);
+
+
+
+
+
+        }
+    }
+}

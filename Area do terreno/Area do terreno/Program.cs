01cc8c71ca0aa4d2548b28444aa1d3b212f48d1f
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-05 09:56:08 -0300

    Exercício
    Corrigido no URI

---
+++ Area do terreno/Area do terreno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization
+
+namespace curso 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, valor;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = largura * comprimento;
+            double PrecoMetroQuadrado = area * valor;
+
+            Console.WriteLine("Área do terreno: " + area.ToString("F2"), CultureInfo.InvariantCulture + " m²");
+            Console.WriteLine("Valor do terreno: " + PrecoMetroQuadrado.ToString("F2"), CultureInfo.InvariantCulture + " reais.");
+
+
+
+
+
+
+
+        }
+    }
+}

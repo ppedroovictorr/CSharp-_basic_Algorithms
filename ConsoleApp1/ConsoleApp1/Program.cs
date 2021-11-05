8bce2e86b29f9db09d4384152a1d651b047ee3be
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-05 10:02:45 -0300

    Vetor
    Corrigido no URI

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("DIgite um caracter: ");
+            char ch = char.Parse(Console.ReadLine());
+            Console.Write("Digite um número decimal: ");
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou: " + n1);
+            Console.WriteLine("Você digitou a letra: " + "'" + ch + "'");
+            Console.WriteLine("Você digitou o número: " + n2.ToString("F2", CultureInfo.InvariantCulture) + ". Só foi apresentado duas casas decimais."); ;
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+        }
+    }
+}

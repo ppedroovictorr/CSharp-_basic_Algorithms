4d8ddbe30b0292ee0a4b8c19d85a79ebdae3af5d
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-05 10:09:09 -0300

    Add files via upload

---
+++ Saída de dados/Saída de dados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Saída_de_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double x = 16.8965475;
+            Console.Write("Seu número decimal é: ");
+            Console.WriteLine(x);
+            Console.WriteLine("Arredondamento para duas casas decimais: " + x.ToString("F2"));
+            Console.WriteLine("Arredondamento para três casas decimais: " + x.ToString("F3"));
+            Console.WriteLine("Arredondamento para quatro casas decimais: " + x.ToString("F4") + " Obrigado por rodar esse pgrm!" );
+
+
+        }
+    }
+}

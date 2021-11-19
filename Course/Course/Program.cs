330a4c633a04f8a3cbe69d4e0b49d952fbda8c5b
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-19 15:17:49 -0300

    Add files via upload

---
+++ Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            SByte x = 100;
+            byte n1 = 126;
+            byte n2 = 255;
+            n2++;
+
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+
+            Console.ReadKey();
+        }
+    }
+}

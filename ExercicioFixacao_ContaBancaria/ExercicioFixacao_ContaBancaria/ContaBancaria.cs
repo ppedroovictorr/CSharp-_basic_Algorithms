eb1970a14f3172fe7a3c007ecb0b0b995ce71479
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-11-23 20:47:33 -0300

    Add files via upload

---
+++ ExercicioFixacao_ContaBancaria/ExercicioFixacao_ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioFixacao_ContaBancaria {
+    class ContaBancaria {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.00;
+        }
+
+        // palavra this aproveita o construtor anterior
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) {
+            Deposito(depositoInicial);
+        }
+
+        public override string ToString() {
+            return "Conta: "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia) {
+            Saldo -= quantia;
+            Saldo -= 5.00;
+        }
+
+
+
+    }
+}

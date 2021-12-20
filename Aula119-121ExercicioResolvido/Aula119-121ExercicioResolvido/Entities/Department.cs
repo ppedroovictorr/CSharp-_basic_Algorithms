2ecb5a41a70566642778c280548782bbb02cf45d
Pedro Victor Oliveira <93488775+ppedroovictorr@users.noreply.github.com>
2021-12-20 13:37:10 -0300

    Add files via upload

---
+++ Aula119-121ExercicioResolvido/Aula119-121ExercicioResolvido/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula119_121ExercicioResolvido.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Aula119-121ExercicioResolvido/Aula119-121ExercicioResolvido/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Aula119_121ExercicioResolvido.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
